3ea46b08624866da7737f175be1d40a2205ecfe8
Paulo Viana <pauloc.fviana@hotmail.com>
2021-08-24 15:13:00 -0300

    implementação hr-payroll mock

---
+++ hr-payroll/src/main/java/com/viana/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.viana.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.viana.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+}
