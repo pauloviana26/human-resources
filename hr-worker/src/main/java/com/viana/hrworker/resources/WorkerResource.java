a530f10c4df3a16f671f704f886141c50774f35a
Paulo Viana <pauloc.fviana@hotmail.com>
2021-08-27 11:16:54 -0300

    db config server

--- hr-worker/src/main/java/com/viana/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/viana/hrworker/resources/WorkerResource.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -24,20 +23,11 @@ public class WorkerResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 	
-	@Value("${test.config}")
-	private String testConfig;
-	
 	@Autowired
 	private Environment env;
 
 	@Autowired
 	private WorkerRepository repository;
-	
-	@GetMapping(value = "/configs")
-	public ResponseEntity<Void> getConfigs() {
-		logger.info("CONFIG = " + testConfig);
-		return ResponseEntity.noContent().build();
-	}
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
@@ -48,11 +38,11 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
 		
-		try {
-			Thread.sleep(3000L);
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
+//		try {
+//			Thread.sleep(3000L);
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//		}
 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
