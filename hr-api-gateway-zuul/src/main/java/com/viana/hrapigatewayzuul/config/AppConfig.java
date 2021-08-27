f013f5188d472869e72dba797da0046e37be7ca3
Paulo Viana <pauloc.fviana@hotmail.com>
2021-08-27 09:47:42 -0300

    Informações de segurança no config-server

--- hr-api-gateway-zuul/src/main/java/com/viana/hrapigatewayzuul/config/AppConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/viana/hrapigatewayzuul/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.viana.hrapigatewayzuul.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -7,11 +8,15 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
+	
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+	
 
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("MY-SECRET-KEY");
+		tokenConverter.setSigningKey("jwtSecret");
 		return tokenConverter;
 	}
 	
